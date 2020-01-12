16031465ebf3997b95f570ee5fb75db4eb1fa31c
Mohammed <moohyacc@gmail.com>
2020-01-12 14:20:26 +0300

    cookies to any page

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_menu_app', domain: 'https://moohy.github.io/menu_app'
+    Rails.application.config.session_store :cookie_store, key: '_menu_app'#, domain: 'https://moohy.github.io/menu_app'
   else
     Rails.application.config.session_store :cookie_store, key: '_menu_app' 
   end

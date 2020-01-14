47fe24f520538d50e20f33f1d2ae23b08b7a6819
Mohammed <moohyacc@gmail.com>
2020-01-14 15:48:57 +0300

    fixing cookie and cors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_menu_app', domain: 'http://localhost:3000'
+    Rails.application.config.session_store :cookie_store, key: '_menu_app', domain: 'http://localhost:3000', httponly: false
   else
-    Rails.application.config.session_store :cookie_store, key: '_menu_app' 
+    Rails.application.config.session_store :cookie_store, key: '_menu_app', httponly: false
   end

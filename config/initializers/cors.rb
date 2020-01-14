42bb6122665d62e88a30ef820c6b2a4f27175057
Mohammed <moohyacc@gmail.com>
2020-01-14 16:22:59 +0300

    changing cors link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'https://secure-hollows-28256.herokuapp.com/'
+      origins 'https://secure-hollows-28256.herokuapp.com'
     
       resource '*',
         headers: :any,
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_menu_app', domain: 'https://secure-hollows-28256.herokuapp.com/'
+    Rails.application.config.session_store :cookie_store, key: '_menu_app', domain: 'https://secure-hollows-28256.herokuapp.com'
   else
     Rails.application.config.session_store :cookie_store, key: '_menu_app'
   end
